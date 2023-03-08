9c0679696f97c3d8cf724f0b981bb08698c3cebc
arafat-rimon <rimonarafat2020@gmail.com>
2023-03-08 22:47:38 +0600

    first commit

---
+++ src/main/java/com/example/pharmacy_managemet_system/PharmacyManagemetSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_managemet_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmacyManagemetSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmacyManagemetSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pharmacy_managemet_system/PharmacyManagemetSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pharmacy_managemet_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmacyManagemetSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
